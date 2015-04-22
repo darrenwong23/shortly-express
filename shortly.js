28ff5b50d19d081c5e973aded2e7dc47796833dd
darrenwong23 <darrenwong23@gmail.com>
2015-04-21 19:32:03 -0700

    added passport oauth

--- shortly.js
+++ shortly.js
@@ -12,7 +12,8 @@ var Link = require('./app/models/link');
 var Click = require('./app/models/click');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
 var app = express();
 
 var sha256 = require('sha256');
@@ -29,11 +30,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.use(passport.initialize());
+  app.use(passport.session());
 app.get('/',
 function(req, res) {
   if(req.session.username){
     res.render('index');
   }else{
+
     res.render('login');
   }
 });
@@ -45,10 +57,12 @@ function(req, res) {
 
 app.get('/links',
 function(req, res) {
-  Links.reset().fetch().then(function(links) {
+  Links.reset().query('where','user_id','=', req.session.user_id)
+  .fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
+
 
 app.post('/links',
 function(req, res) {
@@ -70,6 +84,7 @@ function(req, res) {
         }
 
         var link = new Link({
+          user_id: req.session.user_id,
           url: uri,
           title: title,
           base_url: req.headers.origin
@@ -87,6 +102,14 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.get('/logout',
+function(req,res) {
+  req.session.destroy(function(){
+    res.redirect('/login');
+  });
+
+});
+
 app.get('/login',
 function(req, res) {
   res.render('login');
@@ -113,6 +136,7 @@ function(req, res) {
 
         req.session.regenerate(function(){
           req.session.username = username;
+          req.session.user_id = found.get('id');
            console.log("******** > session USERNAME --> " + req.session.username);
             res.redirect('index');
         });
@@ -161,12 +185,91 @@ function(req, res) {
     users.forEach(function(user){console.log(user.attributes)});
   });
       });
-
+      res.redirect('/login');
     }
   });
 
 });
+passport.use(new GitHubStrategy({
+    clientID: "d32f0e28cd7f3299ae5a",
+    clientSecret: "37fdfb1fcb539d83060053b17ec4432cd9653e96",
+    callbackURL: "http://127.0.0.1:4568/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
 
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead
+
+
+      return done(null, profile);
+    });
+  }
+));
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    //         req.session.regenerate(function(){
+
+  var username = req.user._json.login;
+
+    new User({ username: req.user._json.login }).fetch().then(function(found) {
+      if (found) {
+        // res.send(200, found.attributes);
+        console.log("FOUND");
+      } else {
+        var user = new User({
+          username: req.user._json.login,
+          password: req.user._json.id
+        });
+
+        console.log(user.get("username"),user.get("password"));
+
+        user.save().then(function(newUser) {
+        Users.add(newUser);
+        //res.send(200, newLink);
+        //
+        Users.reset().fetch().then(function(users) {
+          users.forEach(function(user){console.log(user.attributes)});
+        });
+
+        });
+
+      }
+    });
+
+    new User({ username: req.user._json.login }).fetch().then(function(found) {
+      console.log("*********************** > " + found);
+      if (found) {
+        var tempUser = new User(found.attributes);
+        console.log(tempUser.check(req.user._json.id))
+        if(tempUser.check(req.user._json.id)){
+
+          req.session.regenerate(function(){
+            req.session.username = req.user._json.login;
+            req.session.user_id = found.get('id');
+             console.log("******** > session USERNAME --> " + req.session.username);
+              res.redirect('/');
+          });
+
+          }
+        }
+       else {
+        console.log('login fail');
+
+      }
+    });
+
+    console.log("success");
+
+  });
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
